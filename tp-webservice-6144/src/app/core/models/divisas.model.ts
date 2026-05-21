7b01866da2524dd3cfa0dcd4c946fab5aa90a400
Gutierrez Nanda, Mauro Nahuel <39233689@fi.unju.edu.ar>
2026-05-21 15:32:55 -0300

    Actualizar README para reflejar que el proyecto está en la raíz

--- tp-webservice-6144/src/app/core/models/divisas.model.ts
+++
@@ -1,7 +0,0 @@
-export interface ConversionResponse {
-  success: boolean;
-  query: { from: string; to: string; amount: number };
-  info: { timestamp: number; rate: number };
-  date: string;
-  result: number;
-}
